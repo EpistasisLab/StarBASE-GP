9b498fb0a5bb5d7418a6f11a3040ad55d3223aaa
Attri <attrighosh@gmail.com>
2026-04-23 13:49:56 -0700

    added l1_wt as an user parameter with variable alpha

--- Source/K2/ray_utils.py
+++ Source/K2/ray_utils.py
@@ -670,8 +670,8 @@ def ray_eval_pipeline_r2(component_map: Dict[snp_t, Dict],
         feature_ratio = num_joint_features / num_base_features
 
         # Dynamically scale the alpha penalty based on the true feature ratio
-        #alpha_joint = 1.0 * feature_ratio
-        alpha_joint = 1.0
+        alpha_joint = 1.0 * feature_ratio
+        #alpha_joint = 1.0
 
         joint_regressor = sm.OLS(y_train_centered_scaled, sm.add_constant(X_joint_train, has_constant='add')) # uses the training data, note that the main effects and interactions are already centered together to ensure they are on the same scale for regularization
         joint_results = joint_regressor.fit_regularized(L1_wt=l1_wt, alpha=alpha_joint) # alpha is a hyperparameter that controls the strength of regularization, can be tuned if needed but 0.1 is a common starting point for ridge regression
