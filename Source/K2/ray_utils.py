d81b960021252195b298165f804acf560aa244c4
Attri <attrighosh@gmail.com>
2026-04-23 09:14:40 -0700

    checking alpha=1 for joint model

--- Source/K2/ray_utils.py
+++ Source/K2/ray_utils.py
@@ -671,8 +671,9 @@ def ray_eval_pipeline_r2(component_map: Dict[snp_t, Dict],
         alpha_joint = 1.0 * feature_ratio
 
         joint_regressor = sm.OLS(y_train_centered_scaled, sm.add_constant(X_joint_train, has_constant='add')) # uses the training data, note that the main effects and interactions are already centered together to ensure they are on the same scale for regularization
-        joint_results = joint_regressor.fit_regularized(L1_wt=0.0, alpha=alpha_joint) # alpha is a hyperparameter that controls the strength of regularization, can be tuned if needed but 0.1 is a common starting point for ridge regression
-
+        #joint_results = joint_regressor.fit_regularized(L1_wt=0.0, alpha=alpha_joint) # alpha is a hyperparameter that controls the strength of regularization, can be tuned if needed but 0.1 is a common starting point for ridge regression
+        joint_results = joint_regressor.fit_regularized(L1_wt=0.0, alpha=1) # alpha is a hyperparameter that controls the strength of regularization, can be tuned if needed but 0.1 is a common starting point for ridge regression
+        
         # Score on training data (Model 1 train R²)
         joint_pred_train = joint_results.predict(sm.add_constant(X_joint_train, has_constant='add'))
         joint_r2_train = r2_score(y_train_centered_scaled, joint_pred_train)
